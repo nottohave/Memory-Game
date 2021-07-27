4b2d5519a08447f0944ba90399e6bdd3ba7ebf7a
nottohave <nottohave@gmail.com>
2021-07-26 18:47:58 -0700

    third commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Title - [Memory Game](https://nottohave.github.io/Memory-Game/)
 
-I learnt to build a memory game using Javascript. This is inspired from Ania Kubow. I will style it more into my personal style.
+I learnt to build a memory game using Javascript. This is inspired from [Ania Kubow](https://github.com/kubowania/memory-game). I will style it more into my personal style.
 
 # What I learned
 
@@ -14,3 +14,9 @@ appendChild()
 math.random()
 sort()
 For loops
+
+# Update
+
+7/24/21: display nothing when user reselect matched cards.
+7/26/21: matching cards or unmatched cards will not display alerts.
+Shorten the time of checking flipped cards.
--- app.js
+++ app.js
@@ -66,10 +66,10 @@ document.addEventListener('DOMContentLoaded', () => {
     // create my deck, link the card to my blank img
     function createDeck() {
         for (let i = 0; i < cardArray.length; i++) {
-            var card = document.createElement('img')
-            card.setAttribute('src', './img/blank.png')
+            var card = document.createElement('img') // create each card with the element img
+            card.setAttribute('src', './img/blank.png') // give each card an img source
             card.setAttribute('data-id', i) // give each card an id
-            card.addEventListener('click', flipCard) // flip the card event
+            card.addEventListener('click', flipCard) // add the flip the card event
             grid.appendChild(card) // place the card into the grid div
         }
     }
@@ -80,34 +80,38 @@ document.addEventListener('DOMContentLoaded', () => {
         const optionOneId = cardsChosenId[0];
         const optionTwoId = cardsChosenId[1];
         // show white for two cards match with img name, set an alert
-        // otherwise, show  blank, set a different alert
+        // otherwise, show blank, set a different alert
         if (cardsChosen[0] === cardsChosen[1]) {
-            alert('You found a match')
-            cards[optionOneId].setAttribute('src', './img/white.png');
-            cards[optionTwoId].setAttribute('src', './img/white.png');
+            cards[optionOneId].removeAttribute('data-id');
+            cards[optionTwoId].removeAttribute('data-id');
             cardsWon.push(cardsChosen);
         } else {
+            console.log(cards[optionOneId].getAttribute('data-id'));
+            console.log(cards[optionTwoId].getAttribute('data-id'));
             cards[optionOneId].setAttribute('src', './img/blank.png');
             cards[optionTwoId].setAttribute('src', './img/blank.png');
-            alert('Sorry, try again')
         }
         cardsChosen = [];
         cardsChosenId = [];
-        resultDisplay.textConet = cardsWon.length;
+        resultDisplay.textContent = cardsWon.length;
         if (cardsWon.length === cardArray.length / 2) {
-            resultDisplay.textContent = "congratulation! You found them all!"
+            resultDisplay.textContent = "Congratulation! You found them all!"
         }
     }
 
     //flip your card
     function flipCard() {
-        var cardId = this.getAttribute('data-id')
+        var cardId = this.getAttribute('data-id');
+        // after a match found, if player click on the same spots, display nothing
+        if (cardArray[cardId] === undefined) {
+            return "";
+        }
         cardsChosen.push(cardArray[cardId].name)
         cardsChosenId.push(cardId)
         this.setAttribute('src', cardArray[cardId].img)
         // only 2 cards in the array, then check the match
         if (cardsChosen.length === 2) {
-            setTimeout(checkForMatch, 500);
+            setTimeout(checkForMatch, 150);
         }
     }
 
